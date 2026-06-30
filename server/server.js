d3121fc6c7a697f90e30d53633822c72e537fe15
mohamedrafeek-mr <rafirafibai@gmail.com>
2026-06-30 15:10:12 +0530

    Allow Vercel frontend CORS

--- server/server.js
+++ server/server.js
@@ -33,10 +33,26 @@ transporter.verify((error, success) => {
   }
 })
 
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  'http://localhost:5173',
+  'http://127.0.0.1:5173',
+  'https://stackers-maina.vercel.app',
+  'https://www.stackers-maina.vercel.app'
+].filter(Boolean)
+
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials: true
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin) || /https:\/\/.*\.vercel\.app$/.test(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('CORS not allowed for this origin'))
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }))
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ extended: true, limit: '50mb' }))
